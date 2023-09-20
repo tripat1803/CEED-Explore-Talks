1346f0b441c168f03185f825c2a44c9659cc8c18
Lokesh <lokeshgargadv@gmail.com>
2023-09-20 12:38:27 +0530

    20-09-2023

---
+++ src/explore_talks.js
@@ -0,0 +1,18 @@
+import Nav from "./Nav.js";
+
+window.nav = new Nav();
+
+document.documentElement.setAttribute("theme", "dark");
+// document.documentElement.setAttribute("theme", "light");
+
+document.getElementsByClassName("start-padding")[0].style.height =
+  document.getElementById("navbar").clientHeight + "px";
+
+document.getElementsByTagName("body")[0].onload = (ev) => {
+  setTimeout(() => {
+    document.getElementById("loader").style.opacity = 0;
+    setTimeout(() => {
+      document.getElementById("loader").style.display = "none";
+    }, 2000);
+  }, 2000);
+};
