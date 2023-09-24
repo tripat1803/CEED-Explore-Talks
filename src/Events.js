383a93a3130f402bc89e6f8ee515b946189ac9ec
Lokesh <lokeshgargadv@gmail.com>
2023-09-24 14:50:06 +0530

    22-09-2023

--- src/Events.js
+++ src/Events.js
@@ -5,13 +5,34 @@ export default class Events {
     this.eventDetailBox = document.getElementById("event-detail-box");
     this.eventDialogBox = document.getElementById("event-dialog-box");
 
+    this.updateEventsBox();
+
+    this.eventActiveDates = document.getElementsByClassName("date active");
+    console.log(this.eventActiveDates);
+    for (let i of this.eventActiveDates) {
+      i.addEventListener("mouseenter", (ev) => {
+        this.eventDetailBox.style.width = `20rem`;
+        this.eventDetailBox.style.opacity = `1`;
+      });
+      i.addEventListener("mouseleave", (ev) => {
+        this.eventDetailBox.style.width = `0rem`;
+        this.eventDetailBox.style.opacity = `0`;
+      });
+    }
+
     window.addEventListener("mousemove", (ev) => {
       this.updateEventDetailBox(ev);
     });
   }
 
+  updateEventsBox() {}
+
   updateEventDetailBox(event) {
-    this.eventDetailBox.style.top = `${event.clientY}px`;
-    this.eventDetailBox.style.left = `${event.clientX}px`;
+    if (window.innerWidth / 2 < event.clientX + 5) {
+      this.eventDetailBox.style.left = `calc(${event.clientX - 5}px - 20rem)`;
+    } else {
+      this.eventDetailBox.style.left = `${event.clientX + 5}px`;
+    }
+    this.eventDetailBox.style.top = `${event.clientY + 5}px`;
   }
 }
